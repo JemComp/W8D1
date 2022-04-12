bf4f9555fdf57b93156cc3db0e661c11627b297f
jeremy <jeremypietila@gmail.com>
2022-04-11 17:02:21 -0700

    p03

--- skeleton (8)/skeleton/bin/p01_basic_server.rb
+++ skeleton (8)/skeleton/bin/p01_basic_server.rb
@@ -4,11 +4,11 @@ app = Proc.new do |env|
     req = Rack::Request.new(env)
     res = Rack::Response.new
     res['Content-Type'] = 'text/html'
-    res.write("Hello world!")
+    res.write("#{req.fullpath}")
     res.finish
   end
 
-  
+
 Rack::Server.start(
     app: app,
     Port: 3000
